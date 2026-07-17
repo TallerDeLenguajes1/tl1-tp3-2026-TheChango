547beb7808641e2d813bf234da70c0d4021ef86f
TheChango <cuencasnegro@gmail.com>
2026-07-17 19:56:28 -0300

    parte uno check

---
+++ ArregloDeNombres.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int NOMBRES = 50;
+void mostrarNombres(char *listasNombres[], int cantidad) {
+    printf("\nLos nombres ingresados son:\n");
+  
+    for (int i = 0; i < cantidad; i++) {
+        printf("%s", listasNombres[i]);
+    }
+}
+
+int main() {
+    char *listasNombres[5];
+    char auxiliar[NOMBRES];
+
+    for (int i = 0; i < 5; i++) {
+        printf("Ingrese el nombre %d: ", i + 1);
+        fgets(auxiliar, NOMBRES, stdin);
+        int numero = strlen(auxiliar);
+        listasNombres[i] = (char*)malloc((numero + 1) * sizeof(char));
+        strcpy(listasNombres[i], auxiliar);
+    }
+    mostrarNombres(listasNombres, 5);
+    return 0;
+}
